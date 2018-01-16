7e3cb66da5cb105dc9219bba3cf8232ea1be89fe
Srlimao <w.back93@gmail.com>
2018-01-16 01:10:46 -0200

    01_10

--- CleanCardGame/Scenes/Battle_Scene.gd
+++ CleanCardGame/Scenes/Battle_Scene.gd
@@ -8,10 +8,24 @@ func _ready():
 	pass
 	
 func _process(delta):
-	if Input.is_action_pressed("ui_accept"):
-			player_node = player_scn.instance()
-			add_child(player_node)
-			player_node.set_global_pos(Vector2() + get_global_mouse_pos())
+	
+#	if Input.is_action_pressed("ui_accept"):
+#			Player.new(10)
+#			player_node = player_scn.instance()
+#			add_child(player_node)
+#			player_node.set_global_pos(Vector2() + get_global_mouse_pos())
 	pass
 
+class Player:
+	var hp
+	var bp
+	var name
+	var weaponId
 
+
+	
+	func _init(var player_id):
+		
+		
+		#player_node.set_global_pos(Vector2() + get_global_mouse_pos())
+		pass
