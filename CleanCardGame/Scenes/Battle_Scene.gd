4595b015a4d78ccd1ad2d338121f5bc80b184d08
PC-Sispro <wbsilveira@sispro.com.br>
2018-01-16 15:58:21 -0200

    16_01

--- CleanCardGame/Scenes/Battle_Scene.gd
+++ CleanCardGame/Scenes/Battle_Scene.gd
@@ -3,18 +3,30 @@ extends Node
 var player_scn = preload("res://Scenes/Objects/Player.tscn")
 var player_node
 
-func _ready():
+func _ready(): 
 	set_process(true)
 	pass
 	
 func _process(delta):
 	
-#	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("ui_accept"):
+		save_game()
 #			Player.new(10)
 #			player_node = player_scn.instance()
 #			add_child(player_node)
 #			player_node.set_global_pos(Vector2() + get_global_mouse_pos())
 	pass
+
+func save_game():
+	var savegame = File.new()
+	savegame.open("res://savegame.save", File.WRITE)
+	var saveplayer = {
+		id = 1,
+		hp = 10,
+		name = "Will"
+	}
+	savegame.store_line(saveplayer.to_json())
+	savegame.close()
 
 class Player:
 	var hp
@@ -29,3 +41,8 @@ class Player:
 		
 		#player_node.set_global_pos(Vector2() + get_global_mouse_pos())
 		pass
+
+
+func _on_Button_pressed():
+	get_node("Player").atk(1)
+	pass # replace with function body
