687029bcd7b01505a1a5130cd84ce461dddfd13e
Srlimao <w.back93@gmail.com>
2018-01-17 00:08:51 -0200

    17_01-00_00

--- CleanCardGame/Scenes/Battle_Scene.gd
+++ CleanCardGame/Scenes/Battle_Scene.gd
@@ -8,14 +8,9 @@ func _ready():
 	set_process(true)
 	pass
 	
+
 func _process(delta):
 	
-	if Input.is_action_pressed("ui_accept"):
-		player_node.move()
-#			Player.new(10)
-#			player_node = player_scn.instance()
-#			add_child(player_node)
-#			player_node.set_global_pos(Vector2() + get_global_mouse_pos())
 	pass
 
 func save_game():
@@ -29,21 +24,7 @@ func save_game():
 	savegame.store_line(saveplayer.to_json())
 	savegame.close()
 
-class Player:
-	var hp
-	var bp
-	var name
-	var weaponId
 
-
-	
-	func _init(var player_id):
-		
-		
-		#player_node.set_global_pos(Vector2() + get_global_mouse_pos())
-		pass
-
-
-func _on_Button_pressed():
-	get_node("Player").atk(1)
+func _on_Button_button_up():
+	player_node.atk(1, get_node("Enemy"))
 	pass # replace with function body
