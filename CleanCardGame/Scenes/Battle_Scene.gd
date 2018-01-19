acceef5d503de9f60d1b5426c7572409d7fa7da8
PC-Sispro <wbsilveira@sispro.com.br>
2018-01-19 16:52:01 -0200

    19-01-17-00

--- CleanCardGame/Scenes/Battle_Scene.gd
+++ CleanCardGame/Scenes/Battle_Scene.gd
@@ -1,9 +1,15 @@
 extends Node
+signal change_turn
 
 var player_scn = preload("res://Scenes/Objects/Player.tscn")
 var player_node
+onready var enemy_node = get_node("Enemy")
+enum TURN{player,enemy}
+var curr_turn
+
 
 func _ready(): 
+	curr_turn = TURN.player
 	player_node = get_node("Player")
 	set_process(true)
 	pass
@@ -16,16 +22,29 @@ func _process(delta):
 func save_game():
 	var savegame = File.new()
 	savegame.open("res://savegame.save", File.WRITE)
-	var saveplayer = {
+	var saveplayer = {"players":[{
 		id = 1,
-		hp = 10,
+		hp = player_node.get_hp(),
+		armor = player_node.get_armor(),
 		name = "Will"
-	}
-	savegame.store_line(saveplayer.to_json())
+	},{
+		id = 2,
+		hp = enemy_node.get_hp(),
+		armor = enemy_node.get_armor(),
+		name = "Bixo"
+	}]}
+	savegame.store_string(saveplayer.to_json())
 	savegame.close()
 
 
 func _on_Button_button_up():
-	player_node.atk(1, get_node("Enemy"))
-	get_node("Camera2D").shake(0.2)
+	if curr_turn == TURN.player:
+		player_node.atk(1, enemy_node)
+		curr_turn = TURN.enemy
+		emit_signal("change_turn")
+	pass # replace with function body
+
+
+func _on_TextureButton_pressed():
+	save_game()
 	pass # replace with function body
